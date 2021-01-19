e5af13f101d1645ebbab272a0835d622b497d641
oa <omar.aghallaj@student.ehb.be>
2021-01-19 17:32:32 +0100

    player connection

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,2 +1,22 @@
 "use-strict"
-console.log("hi");
+
+window.onload = () => {
+
+    console.log("code runs");
+    pianoKey();
+}
+
+function pianoKey() {
+    const model = new mm.MusicVAE(
+        'https://storage.googleapis.com/magentadata/js/checkpoints/music_vae/trio_4bar');
+    const player = new mm.Player();
+    player.start(TWINKLE_TWINKLE);
+    player.stop();
+
+    $(".key").click(function() {
+        let key = $(this)
+        let keyNote = key.attr("data-note");
+        console.log(keyNote);
+      });
+
+}
