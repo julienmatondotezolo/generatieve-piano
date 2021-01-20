0e774d19b438011fa7ee802a7302fa2c5444be5d
oa <omar.aghallaj@student.ehb.be>
2021-01-20 12:49:58 +0100

    generate music on keypress

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,6 +1,26 @@
 "use-strict"
 
-$(".key").click(function (e) { 
-    e.preventDefault();
+
+window.onload = () =>{
+    //test
     
-});
+
+     
+    // const genie = new mm.PianoGenie(CONSTANTS.GENIE_CHECKPOINT);
+    // console.log(genie);
+      const mvae = new mm.MusicVAE('https://storage.googleapis.com/magentadata/js/checkpoints/music_vae/trio_4bar');
+      const player = new mm.Player();
+      console.log(player);
+      
+      $(".key").click(function() {
+        let key = $(this);
+        mvae.initialize().then(() => {
+            key.forEach(element => {
+                mvae.sample(2).then((samples) => player.start(samples[element]));
+            });
+            
+          });
+        console.log(key.attr("data-note"));
+      });
+
+}
