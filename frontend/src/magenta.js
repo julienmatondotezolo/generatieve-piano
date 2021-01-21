db1c96f97ebdfba6f9f8172cb613150145ef4634
Julien Matondo <Julien.matondo-tezolo@student.ehb.be>
2021-01-21 22:24:04 +0100

    fix comments

--- frontend/src/magenta.js
+++ frontend/src/magenta.js
@@ -5,21 +5,22 @@ let newKeyData;
 
 export function playNotes(keyData) {
 
-    let TWINKLE = {
+    let pitchLevel = 30;
 
+    let TWINKLE = {
         notes: [{
-            pitch: 50,
+            pitch: pitchLevel,
             startTime: 0.0,
             endTime: 0.1
-        }],
+        }]
     };
 
     if (keyData.indexOf('w') > -1) {
         newKeyData = keyData.replace('w', '');
-        TWINKLE.notes[0].pitch = 50 + parseInt(newKeyData);
+        TWINKLE.notes[0].pitch = pitchLevel + parseInt(newKeyData);
     } else {
         newKeyData = keyData.replace('b', '');
-        TWINKLE.notes[0].pitch = 70 + parseInt(newKeyData);
+        TWINKLE.notes[0].pitch = pitchLevel + 20 + parseInt(newKeyData);
         keyData.replace('b', '');
     }
 
