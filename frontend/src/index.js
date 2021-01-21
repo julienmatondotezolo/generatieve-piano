8917efebc3f5f077de53e29e3e57397a050172c9
Julien Matondo <Julien.matondo-tezolo@student.ehb.be>
2021-01-21 17:45:36 +0100

    fix lagging  notes

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,43 +1,37 @@
 "use strict";
 
-
 let clicked = false;
 let players = new mm.Player();
 let noteSeq = [];
+let counter = 0;
+let newKeyData;
 
- $(".key").click(function() {
+$(".key").click(function () {
     let keyData = $(this).attr('data-note');
     playNotes(keyData);
-}); 
+});
 
-
-$(".key").mouseup(function() {
+$(".key").mouseup(function () {
     clicked = false;
 
-}).mousedown(function() {
+}).mousedown(function () {
     clicked = true;
     let keyData = $(this).attr('data-note');
-            playNotes(keyData);
+    playNotes(keyData);
 
-    $('.key').mouseenter(function(e) {
+    $('.key').mouseenter(function (e) {
         if ($(".key:hover").length !== 0 && clicked) {
-
             let keyData = $(this).attr('data-note');
             playNotes(keyData);
             // holdNote();
         }
-    }).mouseleave(function() {
-
+    }).mouseleave(function () {
         // resetNote();
     });
+
 });
 
-let counter = 0;
-
 function playNotes(keyData) {
-
-    let keyNumber;
-
 
     let TWINKLE = {
 
@@ -48,26 +42,14 @@ function playNotes(keyData) {
         }],
     };
 
-
     if (keyData.indexOf('w') > -1) {
-
-        let newKeyData = keyData.replace('w', '');
-        console.log(newKeyData);
+        newKeyData = keyData.replace('w', '');
         TWINKLE.notes[0].pitch = 50 + parseInt(newKeyData);
-        console.log("white");
-
-
     } else {
-
-        let newKeyData = keyData.replace('b', '');
-        console.log(newKeyData);
+        newKeyData = keyData.replace('b', '');
         TWINKLE.notes[0].pitch = 70 + parseInt(newKeyData);
-
         keyData.replace('b', '');
-        console.log('black');
     }
-
-
 
     if (counter >= 1) {
         players.stop(TWINKLE);
@@ -75,9 +57,7 @@ function playNotes(keyData) {
 
     counter++;
 
-
     players.start(TWINKLE);
-
 }
 
 /* function holdNote() {
