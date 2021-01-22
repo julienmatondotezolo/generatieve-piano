f52fc8a7457f60b7c1fad5d06fe679e699a7475d
Julien Matondo <Julien.matondo-tezolo@student.ehb.be>
2021-01-22 11:11:04 +0100

    adding autoplay

--- frontend/src/keyboard.js
+++ frontend/src/keyboard.js
@@ -1,9 +1,8 @@
+/*/////////////   IMPORTS   ////////////////*/
+
 import {
     playNotes
 } from './magenta.js';
-
-initWebcam()
-initKeyboard()
 
 /*/////////////   VARIABLES   ////////////////*/
 
@@ -11,6 +10,90 @@ let clicked = false;
 let sec = 0;
 let keyboardColor;
 let keyData;
+
+let noteSeqData = {
+    notes: [{
+            pitch: 60,
+            startTime: 0.0,
+            endTime: 0.5
+        },
+        {
+            pitch: 60,
+            startTime: 0.5,
+            endTime: 1.0
+        },
+        {
+            pitch: 67,
+            startTime: 1.0,
+            endTime: 1.5
+        },
+        {
+            pitch: 67,
+            startTime: 1.5,
+            endTime: 2.0
+        },
+        {
+            pitch: 69,
+            startTime: 2.0,
+            endTime: 2.5
+        },
+        {
+            pitch: 69,
+            startTime: 2.5,
+            endTime: 3.0
+        },
+        {
+            pitch: 67,
+            startTime: 3.0,
+            endTime: 4.0
+        },
+        {
+            pitch: 65,
+            startTime: 4.0,
+            endTime: 4.5
+        },
+        {
+            pitch: 65,
+            startTime: 4.5,
+            endTime: 5.0
+        },
+        {
+            pitch: 64,
+            startTime: 5.0,
+            endTime: 5.5
+        },
+        {
+            pitch: 64,
+            startTime: 5.5,
+            endTime: 6.0
+        },
+        {
+            pitch: 62,
+            startTime: 6.0,
+            endTime: 6.5
+        },
+        {
+            pitch: 62,
+            startTime: 6.5,
+            endTime: 7.0
+        },
+        {
+            pitch: 60,
+            startTime: 7.0,
+            endTime: 8.0
+        },
+    ]
+};
+
+/*/////////////   FUNCTION INITIALISATIONS   ////////////////*/
+
+initWebcam()
+initKeyboard()
+
+document.querySelector('button').addEventListener('click', async () => {
+    console.log('audio is ready')
+    await autoplayNotes(noteSeqData, '');
+})
 
 /*/////////////   CLICK FUNCTIONS ON KEY   ////////////////*/
 
@@ -26,7 +109,7 @@ $(".key").mouseup(function () {
     playNotes(keyData);
 
     keyboardColor = $('.keyboard').attr('data-color');
-    addColorToKey(this, keyboardColor)
+    addColorToKey(this, keyboardColor, false)
     changeKeyStatus($(this).attr('data-active'), this)
     // window.myTimer = setInterval(createNote, 25, $(this).width(), $(this).position().left)
     createNote($(this).width(), $(this).position().left)
@@ -38,7 +121,7 @@ $(".key").mouseup(function () {
             playNotes(keyData);
 
             keyboardColor = $('.keyboard').attr('data-color');
-            addColorToKey(this, keyboardColor)
+            addColorToKey(this, keyboardColor, false)
             createNote($(this).width(), $(this).position().left)
         }
     }).mouseleave(function () {
@@ -76,40 +159,40 @@ function initKeyboard() {
 function generateKeyboard() {
     $('.keyboard').empty();
 
-    let keyLength = 30
+    let keyLength = 80
 
-    for (let i = 0; i < keyLength; i++) {
+    for (let i = 50; i < keyLength; i++) {
         let note = i;
-        generateKey(note, keyWidth(keyLength))
+        generateKey(note, keyWidth(30), i)
     }
 
-    $(".key[data-note=b2]").remove();
-    $(".key[data-note=b3]").remove();
-    $(".key[data-note=b4]").remove();
-    $(".key[data-note=b8]").remove();
-    $(".key[data-note=b9]").remove();
-    $(".key[data-note=b13]").remove();
-    $(".key[data-note=b14]").remove();
-    $(".key[data-note=b17]").remove();
-    $(".key[data-note=b20]").remove();
-    $(".key[data-note=b24]").remove();
-    for (let i = 25; i < 30; i++) {
-        $(`.key[data-note=b${i}]`).remove();
-    }
+    // $(".key[data-note=b2]").remove();
+    // $(".key[data-note=b3]").remove();
+    // $(".key[data-note=b4]").remove();
+    // $(".key[data-note=b8]").remove();
+    // $(".key[data-note=b9]").remove();
+    // $(".key[data-note=b13]").remove();
+    // $(".key[data-note=b14]").remove();
+    // $(".key[data-note=b17]").remove();
+    // $(".key[data-note=b20]").remove();
+    // $(".key[data-note=b24]").remove();
+    // for (let i = 65; i < 70; i++) {
+    //     $(`.key[data-note=b${i}]`).remove();
+    // }
     let keyboard = " * Keyboard loaded * "
     console.log("%c" + keyboard, "background: #f0047f; color: #fff")
 }
 
 /*/////////////   GENERATE KEYS   ////////////////*/
 
-function generateKey(keyNote, keyLength) {
+function generateKey(keyNote, keyLength, iteration) {
     $('.keyboard').append(`
 
         <div class="key white unselectable" data-note="w${keyNote}" data-active="false">
             <p>W${keyNote}</p>
         </div>
 
-        <div class="key black unselectable" data-note="b${keyNote}" data-active="false" style="left: ${1 + keyNote * 4}%">
+        <div class="key black unselectable" data-note="b${keyNote}" data-active="false" style="margin-left: ${iteration + 20 * 3}px">
             <p>B${keyNote}</p>
          </div>
     `);
@@ -126,6 +209,21 @@ function changeKeyStatus(keyStatus, element) {
     }
 }
 
+function getKeyNumber(key) {
+    let keyNumber;
+    let getKeyNote = key.attr('data-note');
+
+    if (getKeyNote.indexOf('w') > -1) {
+        keyNumber = getKeyNote.replace('w', ' ')
+    }
+
+    if (getKeyNote.indexOf('b') > -1) {
+        keyNumber = getKeyNote.replace('b', ' ')
+    }
+
+    return keyNumber
+}
+
 /*/////////////   RESPONSIVE KEY WITDH   ////////////////*/
 
 function keyWidth(keysLength) {
@@ -136,26 +234,67 @@ function keyWidth(keysLength) {
 
 /*/////////////   CLICKED KEY FUNCTIONS   ////////////////*/
 
-function addColorToKey(element, color) {
+function addColorToKey(element, color, autoplay, endTime) {
     $(element).css('background-color', color)
-    $(element).mouseout(function () {
+
+    if (autoplay) {
         setTimeout(function () {
-            $(element).removeClass("colorBg");
-            $(element).css('background-color', 'keyboardColor')
-        }, 1000);
-    });
+            $(element).css('background-color', '')
+        }, endTime);
+    } else {
+        $(element).mouseout(function () {
+            setTimeout(function () {
+                $(element).css('background-color', '')
+            }, 1000);
+        });
+    }
+
 }
 
 /*/////////////   GENERATE NOTES   ////////////////*/
 
-function createNote(width, positionLeft) {
+function createNote(width, positionLeft, height) {
     keyboardColor = $('.keyboard').attr('data-color');
+    let keyHeight;
+
+    if (height) {
+        keyHeight = height;
+    } else {
+        keyHeight = 5;
+    }
 
     $('.notes').append(`
-        <div class="note-block" style="left: ${positionLeft}px; height: ${5}0px; width: ${width}px; background-color: ${keyboardColor} !important"></div>
+        <div class="note-block" style="left: ${positionLeft}px; height: ${keyHeight}0px; width: ${width}px; background-color: ${keyboardColor} !important"></div>
     `);
 
     setTimeout(function () {
         $(`.note-block:nth-child(1)`).remove();
     }, 5000);
+}
+
+function calculateHeight(start, end) {
+    let height = end - start;
+    return height;
+}
+
+async function autoplayNotes(noteSeq, keyboardColor) {
+
+    let notesArr = []
+    let height;
+
+    for (const notes of await noteSeq.notes) {
+        let matchKey = $(".keyboard").find(`.key[data-note='w${notes.pitch}']`)
+        keyData = matchKey.attr('data-note');
+
+        notesArr.push(notes)
+
+        setTimeout(() => {
+            addColorToKey(matchKey, '#f0047f', true, notes.startTime + 1000)
+            height = calculateHeight(notes.startTime * 10, notes.endTime * 10)
+            createNote(matchKey.width(), matchKey.position().left, height)
+        }, notes.startTime * 1000);
+    }
+
+    await playNotes(keyData, notesArr);
+
 }
--- frontend/src/magenta.js
+++ frontend/src/magenta.js
@@ -3,41 +3,50 @@
 let players = new mm.Player();
 let newKeyData;
 
-export function playNotes(keyData) {
-
+export async function playNotes(keyData, notesArr) {
+    players = await new mm.SoundFontPlayer('https://storage.googleapis.com/magentadata/js/soundfonts/sgm_plus');
+    
+    let MIDI;
     let pitchLevel = 30;
 
-    let TWINKLE = {
-        notes: [{
-            pitch: pitchLevel,
-            startTime: 0.0,
-            endTime: 0.1
-        }]
-    };
+    if (notesArr) {
+        MIDI = {
+            notes: notesArr
+        };    
+    } else {
+        MIDI = {
+            notes: [{
+                pitch: pitchLevel,
+                startTime: 0.0,
+                endTime: 0.1
+            }]
+        };
+    }
 
     if (keyData.indexOf('w') > -1) {
         newKeyData = keyData.replace('w', '');
-        TWINKLE.notes[0].pitch = pitchLevel + parseInt(newKeyData);
+        MIDI.notes[0].pitch = parseInt(newKeyData);
     } else {
         newKeyData = keyData.replace('b', '');
-        TWINKLE.notes[0].pitch = pitchLevel + 20 + parseInt(newKeyData);
-        keyData.replace('b', '');
+        MIDI.notes[0].pitch = parseInt(newKeyData) - 20;
     }
 
     if (players) {
-        players.stop(TWINKLE);
+        players.stop(MIDI);
+        console.log('player: ', 'stopped')
     } else {
         players.resume()
+        console.log('player: ', 'resume')
     }
 
-    players.start(TWINKLE);
+    players.start(MIDI);
 }
 
 /* function holdNote() {
-    TWINKLE.notes[0].endTime += 0.1;
-    //console.log(TWINKLE.notes[0].endTime += 1);
+    MIDI.notes[0].endTime += 0.1;
+    //console.log(MIDI.notes[0].endTime += 1);
 }
 
 function resetNote() {
-    TWINKLE.notes[0].endTime = 0.1;
+    MIDI.notes[0].endTime = 0.1;
 } */
