e968f4986ba382172a34b1b442fcf8497eee3442
Julien Matondo <Julien.matondo-tezolo@student.ehb.be>
2021-01-22 12:11:06 +0100

    changing sound font player + exporting

--- frontend/src/magenta.js
+++ frontend/src/magenta.js
@@ -1,25 +1,27 @@
 "use strict";
 
 let players = new mm.Player();
+players = new mm.SoundFontPlayer('https://storage.googleapis.com/magentadata/js/soundfonts/sgm_plus');
 let newKeyData;
 
-export function playNotes(keyData) {
+export async function playNotes(keyData) {
+
+    let pitchLevel = 30;
 
     let TWINKLE = {
-
         notes: [{
-            pitch: 50,
+            pitch: pitchLevel,
             startTime: 0.0,
             endTime: 0.1
-        }],
+        }]
     };
 
     if (keyData.indexOf('w') > -1) {
         newKeyData = keyData.replace('w', '');
-        TWINKLE.notes[0].pitch = 50 + parseInt(newKeyData);
+        TWINKLE.notes[0].pitch = pitchLevel + parseInt(newKeyData);
     } else {
         newKeyData = keyData.replace('b', '');
-        TWINKLE.notes[0].pitch = 70 + parseInt(newKeyData);
+        TWINKLE.notes[0].pitch = pitchLevel + 20 + parseInt(newKeyData);
         keyData.replace('b', '');
     }
 
