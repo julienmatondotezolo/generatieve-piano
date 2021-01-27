b89a32da759772e1102f1df5532d4de2b0679698
Ayoub <ayoub.deneubourg@student.ehb.be>
2021-01-27 12:33:03 +0100

    new post route, comments, structure & functions

--- backend/server2.js
+++ backend/server2.js
@@ -1,31 +1,36 @@
-/*--------- Variables --------*/
 /* global require */
 
 "use strict";
 
-
+/*--------- Variables --------*/
 const express = require("express");
 const bodyParser = require("body-parser");
 const http = require("http");
 const fs = require("fs");
 const cors = require('cors');
-var path = require('path');
+// const tf = require('@tensorflow/tfjs-node'); // Uncomment for heroku && comment for localhost
+const path = require('path');
 const port = process.env.PORT || 3000;
 const mmcore = require('@magenta/music/node/core');
 const rnn = require('@magenta/music/node/music_rnn');
-const melodyRNN = new rnn.MusicRNN('https://storage.googleapis.com/magentadata/js/checkpoints/music_rnn/melody_rnn'); // Makes a melody from the input
+const melodyRNN = new rnn.MusicRNN('https://storage.googleapis.com/magentadata/js/checkpoints/music_rnn/melody_rnn');
+const app = express();
 
 melodyRNN.initialize();
-const app = express();
 http.Server(app);
 
-app.use(bodyParser.json());
-app.use(
-    bodyParser.urlencoded({
-        extended: true,
-    })
-);
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT, DELETE");
+    res.header(
+        "Access-Control-Allow-Headers",
+        "Origin, X-Requested-With, Content-Type, Accept"
+    );
+    next();
+});
 
+app.use(bodyParser.urlencoded({ extended: true, }));
+app.use(bodyParser.json());
 app.use(cors());
 
 
@@ -39,78 +44,86 @@ app.get("/", (req, res) => {
 
     res.sendFile(path.join(__dirname + '/index.html'));
 
-
 });
 
-// ========== POST MIDI TO NOTES ==========  //
+
+/*TODO:: CHANGE MIDI FILES TO HAVE 20 SEC FILES INSTEAD OF 2000 HOURs;*/
+
+
+// ========== POST EMOTIONS TO NOTES ==========  //
+
 app.post("/emotion-to-notes", async(req, res, next) => {
 
     let emotionArr = req.body;
-    let firstEmotion = await extractEmotion(emotionArr);
+    let firstEmotion = extractEmotion(emotionArr);
     let midiFile = chooseMidiFile(firstEmotion);
 
-    console.log(midiFile);
-    async function emotionsToNotes(emotion) {
+    async function emotionsToNotes() {
+
         let midiBuffer;
-
-        console.log(emotion);
-        //  if (emotion) {
         midiBuffer = fs.readFileSync(midiFile);
-        // }
-        const notes = mmcore.midiToSequenceProto(midiBuffer);
+        const notes = mmcore.midiToSequenceProto(midiBuffer); // Transforms a midi file to an array of notes
         const qns = mmcore.sequences.quantizeNoteSequence(notes, 2); // 2 == steps per quarter
-        melodyRNN.continueSequence(qns, 15, 6) // 15=notes // 6== temperature
+        melodyRNN.continueSequence(qns, 15, 2) // AI continues the sequence depending the midi file // 15 == notes /-/ 2 == temperature
             .then(sample => {
-                console.log("yello");
-                let newArray = [];
 
-                sample.notes.forEach(element => {
-                    let newObject = {
-                        pitch: element.pitch,
-                        startTime: element.quantizedEndStep * 0.3,
-                        endTime: element.quantizedEndStep * 0.3 + 0.3,
-                    };
-                    newArray.push(newObject);
-                });
+                let object = createNotesObject(sample);
 
-                res.send({
-                    newArray,
-                    firstEmotion
-
-                });
+                res.send({ object, firstEmotion });
             });
 
     }
-    await emotionsToNotes(firstEmotion);
+    await emotionsToNotes();
 
 
 });
 
+function createNotesObject(item) { // Create a playable object with the generated  notes of the AI
 
-async function extractEmotion(emotionArr) {
-    let mf = 1;
-    let m = 0;
+    let notes = [];
+
+    item.notes.forEach(element => {
+        let newObject = {
+            pitch: element.pitch,
+            startTime: element.quantizedEndStep * 0.3,
+            endTime: element.quantizedEndStep * 0.3 + 0.3,
+        };
+        notes.push(newObject);
+    });
+
+    let filteredNotes = notes.filter((element) => element.pitch < 124 && element.pitch > 7); // filter the notes that are unplayable
+
+
+    return filteredNotes;
+
+}
+
+
+function extractEmotion(emotionArr) { // Takes the most common emotion of the user that the front-end sends to us
+
+    let counter = 0;
+    let counterTwo = 1;
     let firstEmotion;
     for (let i = 0; i < emotionArr.length; i++) {
 
         for (let j = i; j < emotionArr.length; j++) {
             if (emotionArr[i].emotion === emotionArr[j].emotion)
-                m++;
+                counter++;
 
-            if (mf < m) {
-                mf = m;
+            if (counterTwo < counter) {
+                counterTwo = counter;
                 firstEmotion = emotionArr[i].emotion;
             }
 
         }
-        m = 0;
+        counter = 0;
 
     }
 
     return firstEmotion;
 }
 
-function chooseMidiFile(firstEmotion) {
+function chooseMidiFile(firstEmotion) { // Chooses a midi file depending on what extractEmotion() returns
 
     let x = Math.floor(Math.random() * 5) + 1;
     let midiFile;
@@ -143,3 +156,27 @@ function chooseMidiFile(firstEmotion) {
 
     return midiFile;
 }
+// ========== POST: NOTES TO NOTES ==========  //
+app.post("/notes-to-midi", async(req, res, next) => {
+
+    let notes = req.body;
+    notes.totalTime = notes.notes.length;
+
+    console.log(notes);
+
+
+
+
+    const qns = mmcore.sequences.quantizeNoteSequence(notes, 2); // 2 == steps per quarter
+    melodyRNN.continueSequence(qns, 15, 2) // AI continues the sequence depending the midi file // 15 == notes /-/ 2 == temperature
+        .then(sample => {
+
+            let object = createNotesObject(sample);
+
+            res.send(object);
+
+        });
+
+
+
+});
